79c867024b901208125a2019025beffc8f86f56d
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-03-17 10:31:03 -0300

    first commit

---
+++ src/app/cadastro/cadastro-pessoa/cadastro-pessoa.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from './models/Pessoa';
+
+@Component({
+  selector: 'app-cadastro-pessoa',
+  templateUrl: 'cadastro-pessoa.component.html',
+  styles: []
+})
+export class CadastroPessoaComponent implements OnInit {
+
+  pessoa: Pessoa = new Pessoa;
+  listaPessoa: Pessoa[] = []
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  random() {
+    return Math.floor(Math.random() * 1024);
+  }
+
+  adicionar() {
+    if (this.pessoa.nome === undefined) {
+      return alert('O campo Nome não foi preenchido')
+    }
+    this.pessoa.id = this.random()
+    this.listaPessoa.push(this.pessoa)
+    this.pessoa = new Pessoa;
+  }
+
+  excluir(id) {
+    let pessoaId = this.listaPessoa.findIndex(cd => cd.id === id)
+    this.listaPessoa.splice(pessoaId, 1)
+  }
+
+}
